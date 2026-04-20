26dc821c23a2f58c1162888d105f5e2efaa5eeb3
AshaJenvasu <chaiyawatjenvasu@gmail.com>
2026-04-20 11:48:38 +0700

    Add: start Navbar with simple html

---
+++ creative-market/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import logo from "../assets/logo.svg";
+
+const Navbar = () => {
+  return (
+    <nav>
+      {/* 1. Logo Section */}
+      <div>
+        <img src={logo} alt="logo" />
+      </div>
+      {/* 2. Menu Links Section */}
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Category</li>
+      </ul>
+      {/* 3. Buttons Section */}
+      <div>
+        <button>Login</button>
+        <button>Register</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
