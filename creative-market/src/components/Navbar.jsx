429c002276f462b5102795ca4b85065c27772df1
AshaJenvasu <chaiyawatjenvasu@gmail.com>
2026-04-20 15:58:12 +0700

    Add: Hero Section

--- creative-market/src/components/Navbar.jsx
+++ creative-market/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import logo from "../assets/logo.svg";
-import { Link } from "react-router-dom";
+import logo from "../assets/logos/logo.svg";
+import { Link } from "react-router";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center px-10 py-4 bg-black text-white">
+    <nav className="flex justify-between items-center px-8 py-4 bg-black text-white flex-wrap">
       {/* 1. Logo Section */}
       <Link
         to="/"
@@ -12,7 +12,7 @@ const Navbar = () => {
         <img src={logo} alt="logo" className="h-7 w-auto " />
       </Link>
       {/* 2. Menu Links Section */}
-      <ul className="md:flex item-center gap-4 text-xl font-medium">
+      <ul className="flex items-center gap-3 text-xl h-auto font-medium ">
         <li className="hover:text-gray-400 cursor-pointer transition-all">
           Home
         </li>
@@ -21,12 +21,12 @@ const Navbar = () => {
           About
         </li>
         <span>|</span>
-        <li className="hover:text-gray-400 cursor-pointer transition-all">
+        <li className="hover:text-gray-400 cursor-pointer transition-all ">
           Category
         </li>
       </ul>
       {/* 3. Buttons Section */}
-      <div className="flex item-center gap-4">
+      <div className="flex items-center gap-4">
         <button className="bg-white text-black px-4 py-2 hover:bg-gray-400 cursor-pointer transition-all w-30">
           Login
         </button>
