aa5fa9cbebf77fb6cf169ad847143c7332dfcccb
AshaJenvasu <chaiyawatjenvasu@gmail.com>
2026-04-20 13:43:11 +0700

    Add: Navbar component

--- creative-market/src/components/Navbar.jsx
+++ creative-market/src/components/Navbar.jsx
@@ -2,21 +2,33 @@ import logo from "../assets/logo.svg";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center">
+    <nav className="flex justify-between items-center px-10 py-4 bg-black text-white">
       {/* 1. Logo Section */}
       <div>
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" className="h-7 w-auto cursor-pointer" />
       </div>
       {/* 2. Menu Links Section */}
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Category</li>
+      <ul className="md:flex item-center gap-4 text-xl font-medium">
+        <li className="hover:text-gray-400 cursor-pointer transition-all">
+          Home
+        </li>
+        <span>|</span>
+        <li className="hover:text-gray-400 cursor-pointer transition-all">
+          About
+        </li>
+        <span>|</span>
+        <li className="hover:text-gray-400 cursor-pointer transition-all">
+          Category
+        </li>
       </ul>
       {/* 3. Buttons Section */}
-      <div>
-        <button>Login</button>
-        <button>Register</button>
+      <div className="flex item-center gap-4">
+        <button className="bg-white text-black px-4 py-2 hover:bg-gray-400 cursor-pointer transition-all w-30">
+          Login
+        </button>
+        <button className="bg-black text-white px-4 py-2 hover:bg-gray-400 cursor-pointer transition-all w-30 border">
+          Register
+        </button>
       </div>
     </nav>
   );
